07f52b237c73646fdc3fc84f2b9f220b2b8ac3b3
Juan Mulki <ing.mulkiaguilera@gmail.com>
2023-08-09 18:04:39 -0300

    Add options column

--- src/views/Suscriptions.vue
+++ src/views/Suscriptions.vue
@@ -1,15 +1,22 @@
 <template>
   <div>
-    <h4 class="title">Suscriptions List</h4><b-spinner v-if="busy" variant="primary" label="Spinning"></b-spinner>
+    <h4 class="title">Suscriptions List</h4>
     
-    <b-table hover :items="items">
+    <b-table hover :items="items" :fields="fields">
       <template #cell(price)="data">
         $ {{ data.item.price.toFixed(2) }}
       </template>
       <template #cell(duration)="data">
         {{ data.item.duration }} days
       </template>
+      <template #cell(options) v-if="$session.exists()">
+        o p t i o n s
+      </template>
     </b-table>
+
+    <div class="spinner-div">
+      <b-spinner v-if="busy" variant="primary" label="Spinning"></b-spinner>
+    </div>
   </div>
 </template>
 
@@ -22,10 +29,15 @@ export default {
     return {
       busy: false,
       items: [],
+      fields: ['id','name','description','price','duration'],
     };
   },
   mounted() {
     this.busy = true;
+    if (this.$session.exists()) {
+      this.fields.push('options');
+    }
+
     _axios
       .get("suscriptions")
       .then((response) => {
@@ -49,5 +61,10 @@ export default {
   width: 70%;
   margin: auto;
   border-radius: 10px;
+}
+.spinner-div {
+  margin-top: 10px;
+  width: 100%;
+  text-align: center;
 }
 </style>
