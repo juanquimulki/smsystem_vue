5beee5e9cec9ff7fd84315323565dfa2fec4f65d
Juan Mulki <ing.mulkiaguilera@gmail.com>
2023-08-10 18:39:26 -0300

    Navigation guards

--- src/router/index.js
+++ src/router/index.js
@@ -14,31 +14,26 @@ const routes = [
     path: "/",
     name: "login",
     component: Login,
-    meta: { opcion: 0 },
   },
   {
     path: "/signup",
     name: "signup",
     component: Signup,
-    meta: { opcion: 0 },
   },
   {
     path: "/dashboard",
     name: "dashboard",
     component: Dashboard,
-    meta: { opcion: 0 },
   },
   {
     path: "/suscriptions",
     name: "suscriptions",
     component: Suscriptions,
-    meta: { opcion: 0 },
   },
   {
     path: "/mysuscriptions",
     name: "mysuscriptions",
     component: MySuscriptions,
-    meta: { opcion: 0 },
   },
 ];
 
@@ -46,19 +41,33 @@ const router = new VueRouter({
   routes,
 });
 
-// router.beforeEach((to, from, next) => {
-//   if (to.name !== "login" && !router.app.$session.exists())
-//     next({ name: "login" });
-//   else {
-//     if (to.meta.opcion == 0) {
-//       next();
-//     } else {
-//       let opciones = router.app.$session.get("opciones");
-//       if (opciones.includes(to.meta.opcion)) {
-//         next();
-//       }
-//     }
-//   }
-// });
+router.beforeEach((to, from, next) => {
+  //console.log(to, from);
+  if (to.name !== "login") {
+    if (to.name === "signup") {
+      if (router.app.$session.exists()) {
+        next({ name: "dashboard" });
+      } else {
+        next();
+      }
+    } else {
+      if (to.name === "suscriptions") {
+        next();
+      } else {
+        if (router.app.$session.exists()) {
+          next();
+        } else {
+          next({ name: "login" });
+        }
+      }
+    }
+  } else {
+    if (router.app.$session.exists()) {
+      next({ name: "dashboard" });
+    } else {
+      next();
+    }
+  }
+});
 
 export default router;
